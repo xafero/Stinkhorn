71ea0ddf4bc3c7a47d6d9a225b2e946fcbe67d34
xafero <georg.berendt@outlook.com>
2017-02-19 19:49:37 +0100

    First API class

---
+++ Stinkhorn.Agent.API/IAgentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Stinkhorn.Agent.API
+{
+	[ServiceContract]
+	public interface IAgentService
+	{
+		[OperationContract]
+		string Invoke(string arg);
+	}
+}
