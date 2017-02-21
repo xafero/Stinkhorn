33c914f54a7f770ed1b28577e8a898ae95f091f7
xafero <georg.berendt@outlook.com>
2017-02-21 19:27:13 +0100

    Initial gui client

---
+++ Stinkhorn.Bureau/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stinkhorn.Bureau
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
