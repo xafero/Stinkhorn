9910108c33e7b5d0b9182c93c10e7f9be2282911
xafero <georg.berendt@outlook.com>
2017-02-25 23:34:49 +0100

    Added a dynamic request editor dialog

---
+++ Stinkhorn.Bureau/Controls/RequestDialog.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using Stinkhorn.API;
+
+namespace Stinkhorn.Bureau.Controls
+{
+    public partial class RequestDialog : Form
+    {
+        private IRequest req;
+
+        public RequestDialog() : this(new ServeRequest())
+        {
+        }
+
+        public RequestDialog(IRequest req)
+        {
+            InitializeComponent();
+            this.req = req;
+            Text = req.GetType().Name;
+            propertyGrid1.SelectedObject = req;
+        }
+    }
+}
