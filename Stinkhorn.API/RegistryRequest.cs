e85b742b14a42992065cd32ce3464a7604f608a0
xafero <georg.berendt@outlook.com>
2017-02-25 19:55:09 +0100

    Added registry stuff

---
+++ Stinkhorn.API/RegistryRequest.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Stinkhorn.API
+{
+    public class RegistryRequest : IRequest
+    {
+        public RegistryRoot Root { get; set; }
+        public string Path { get; set; }
+        public string Key { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class RegistryResponse : IResponse
+    {
+        public object Value { get; set; }
+    }
+
+    public enum RegistryRoot
+    {
+        CurrentUser,
+        LocalMachine,
+        ClassesRoot,
+        Users,
+        PerformanceData,
+        CurrentConfig,
+        DynData
+    }
+}
---
+++ Stinkhorn.Core/RegistryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Stinkhorn.API;
+using Microsoft.Win32;
+
+namespace Stinkhorn.Core
+{
+    class RegistryHandler : IMessageHandler<RegistryRequest, RegistryResponse>
+    {
+        public RegistryResponse Process(RegistryRequest input)
+        {
+            var root = (RegistryKey)typeof(Registry).GetField(input.Root + "").GetValue(null);
+            var writable = input.Value != null;
+            using (var key = root.OpenSubKey(input.Path, writable))
+            {
+                var ret = new RegistryResponse { Value = key.GetValue(input.Key) };
+                if (writable)
+                    key.SetValue(input.Key, input.Value);
+                return ret;
+            }
+        }
+    }
+}
