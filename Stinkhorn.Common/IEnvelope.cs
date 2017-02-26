ffd345e39e21dc80e891ebd23f770ab00bac5f22
xafero <georg.berendt@outlook.com>
2017-02-26 20:42:27 +0100

    Added Soap-like envelope

---
+++ Stinkhorn.Common/IEnvelope.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Stinkhorn.API;
+
+namespace Stinkhorn.Common
+{
+    public interface IEnvelope
+    {
+        IParticipant Sender { get; }
+        IParticipant Receiver { get; }
+        IDictionary<string, int> Headers { get; }
+        IMessage Body { get; }
+        IFault Error { get; }
+    }
+
+    public interface IParticipant
+    {
+        ParticipantKind Kind { get; }
+        Guid? Id { get; }
+        string Topic { get; }
+    }
+
+    public enum ParticipantKind
+    {
+        Broadcast, Multicast, Unicast
+    }
+
+    public interface IFault
+    {
+        FaultCode Code { get; }
+        string Description { get; }
+        string Details { get; }
+    }
+
+    public enum FaultCode
+    {
+        VersionMismatch, ParamMismatch, ClientSide, ServerSide
+    }
+}
