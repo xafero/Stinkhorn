a646378004683ac5f3cfa1ad63a6c00c8ddcf383
xafero <georg.berendt@outlook.com>
2017-03-05 18:50:40 +0100

    Made some test

--- Stinkhorn.Test/CommunicationTest.cs
+++ Stinkhorn.Test/CommunicationTest.cs
@@ -1,39 +1,58 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
 using NUnit.Framework;
 using Stinkhorn.Common;
-using Stinkhorn.API;
+using System;
+using System.Threading;
 
 namespace Stinkhorn.Test
 {
     [TestFixture]
     public class CommunicationTest
     {
+        private void TestSomething(ITransfer transfer)
+        {
+            var server = new Thread(ServerLoop);
+            server.Start(transfer);
+            var client = new Thread(ClientLoop);
+            client.Start(transfer);
+            Thread.Sleep(30 * 1000);
+            server.Join();
+            client.Join();
+        }
+
         [Test]
         public void TestBroadcast()
         {
-            using (var client1 = new BrokerClient())
-            using (var client2 = new BrokerClient())
-            {
-                client2.Subscribe<HelloMessage>(m =>
-                {
-                    Assert.Fail(m + "");
-                });
-                client1.Publish(new HelloMessage());
-            }
+            TestSomething(Broadcast.Of());
         }
 
         [Test]
         public void TestMulticast()
         {
-
+            TestSomething(Multicast.Of<CommunicationTest>());
         }
 
         [Test]
         public void TestUnicast()
         {
+            TestSomething(Unicast.Of(Guid.NewGuid()));
+        }
 
+        static void ClientLoop(object state)
+        {
+            var addr = (ITransfer)state;
+            var client = new RabbitBroker();
+            client.Open();
+            Thread.Sleep(3 * 1000);
+            client.Publish(addr, $"Hello, I am a client ({addr})");
+        }
+
+        static void ServerLoop(object state)
+        {
+            var addr = (ITransfer)state;
+            var server = new RabbitBroker();
+            server.Open();
+            Thread.Sleep(2 * 1000);
+            server.Subscribe<string>(addr, (s, m) => Console.WriteLine($"{s} => {m}"));
         }
     }
 }
