9335fb2ab063b8f514eb38f171efbcf63b08f427
xafero <georg.berendt@outlook.com>
2017-03-08 20:26:55 +0100

    Added icon for info request

--- Stinkhorn.Bureau/Icons.Designer.cs
+++ Stinkhorn.Bureau/Icons.Designer.cs
@@ -73,6 +73,16 @@ namespace Stinkhorn.Bureau {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Info {
+            get {
+                object obj = ResourceManager.GetObject("Info", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Logo {
             get {
                 object obj = ResourceManager.GetObject("Logo", resourceCulture);
