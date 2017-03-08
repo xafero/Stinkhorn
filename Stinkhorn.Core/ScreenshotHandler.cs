5c31ce5995156af763ecb59304f1aa3f01c4d75b
xafero <georg.berendt@outlook.com>
2017-03-08 23:21:45 +0100

    Added mac screenshot support

--- Stinkhorn.Core/ScreenshotHandler.cs
+++ Stinkhorn.Core/ScreenshotHandler.cs
@@ -4,6 +4,10 @@ using System.Drawing;
 using Stinkhorn.API;
 
 using ImageFormat = System.Drawing.Imaging.ImageFormat;
+using SProcess = System.Diagnostics.Process;
+using System;
+using System.Linq;
+using System.IO;
 
 namespace Stinkhorn.Core
 {
@@ -13,19 +17,37 @@ namespace Stinkhorn.Core
         public ScreenshotResponse Process(ScreenshotRequest input)
         {
             var images = new List<IImage>();
-            foreach (var screen in Screen.AllScreens)
+            var screens = Screen.AllScreens;
+            try
             {
-                var width = screen.Bounds.Width;
-                var height = screen.Bounds.Height;
-                using (var img = new Bitmap(width, height))
+                foreach (var screen in screens)
                 {
-                    var size = new Size(img.Width, img.Height);
-                    var graph = Graphics.FromImage(img);
-                    var x = screen.Bounds.X;
-                    var y = screen.Bounds.Y;
-                    graph.CopyFromScreen(x, y, 0, 0, size);
+                    var width = screen.Bounds.Width;
+                    var height = screen.Bounds.Height;
+                    using (var img = new Bitmap(width, height))
+                    {
+                        var size = new Size(img.Width, img.Height);
+                        var graph = Graphics.FromImage(img);
+                        var x = screen.Bounds.X;
+                        var y = screen.Bounds.Y;
+                        graph.CopyFromScreen(x, y, 0, 0, size);
+                        var bytes = img.ToBytes(ImageFormat.Png);
+                        images.Add(new PngImage(bytes, img.Width, img.Height));
+                    }
+                }
+            }
+            catch (NotImplementedException)
+            {
+                var tmpFiles = screens.Select(s => Path.GetTempFileName()).ToArray();
+                var files = string.Join(" ", tmpFiles);
+                using (var proc = SProcess.Start("screencapture", $"-C -tpng -x {files}"))
+                    proc.WaitForExit(5 * 1000);
+                foreach (var tmpFile in tmpFiles)
+                {
+                    var img = Image.FromFile(tmpFile);
                     var bytes = img.ToBytes(ImageFormat.Png);
                     images.Add(new PngImage(bytes, img.Width, img.Height));
+                    File.Delete(tmpFile);
                 }
             }
             return new ScreenshotResponse { Screenshots = images };
