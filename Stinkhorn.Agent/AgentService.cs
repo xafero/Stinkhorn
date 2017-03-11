38b6dc7b213ce5c7dbdd1c1ddd297256a072ff0f
xafero <georg.berendt@outlook.com>
2017-03-11 19:07:27 +0100

    Agent side works now without knowing too much

--- Stinkhorn.Agent/AgentService.cs
+++ Stinkhorn.Agent/AgentService.cs
@@ -4,6 +4,7 @@ using Stinkhorn.API;
 using Stinkhorn.Comm;
 using Mono.Addins;
 using System;
+using System.Collections.Generic;
 
 namespace Stinkhorn.Agent
 {
@@ -14,15 +15,19 @@ namespace Stinkhorn.Agent
         public const string MyServiceName = "StinkhornAgent";
 
         RabbitBroker Client { get; set; }
+        IDictionary<Type, IRequestHandler> Handlers { get; set; }
 
         public void Dispose()
         {
             Stop();
             Client = null;
+            Handlers.Clear();
+            Handlers = null;
         }
 
         public void Start()
         {
+            Handlers = new Dictionary<Type, IRequestHandler>();
             log.InfoFormat("Service is starting...");
             var name = GetType().Name;
             var client = new RabbitBroker();
@@ -37,26 +42,36 @@ namespace Stinkhorn.Agent
                 Local = client.LocalEndpoint.ToShortString(),
                 Remote = client.RemoteEndpoint.ToShortString()
             });
+            foreach (var handler in AddinExtensions.GetFiltered<IRequestHandler>())
+            {
+                var intfName = typeof(IRequestHandler<>).Name;
+                var intf = handler.GetType().GetInterface(intfName);
+                var reqType = intf.GetGenericArguments()[0];
+                var meth = GetType().GetMethod(nameof(Subscribe));
+                meth = meth.MakeGenericMethod(reqType);
+                meth.Invoke(this, null);
+                Handlers[reqType] = handler;
+            }
+
+            // TODO ...
             //Subscribe<ScreenshotRequest, ScreenshotResponse>();
-            Subscribe<RegistryRequest, RegistryResponse>();
-            Subscribe<PowerRequest, PowerResponse>();
-            Subscribe<ServeRequest, ServeResponse>();
+            Subscribe<RegistryRequest>();
+            Subscribe<PowerRequest>();
+            Subscribe<ServeRequest>();
             //Subscribe<InfoRequest, InfoResponse>();
         }
 
-        void Subscribe<I, O>()
-            where I : IRequest where O : IResponse
+        public void Subscribe<I>() where I : IRequest
         {
             var cid = Client.Id;
             var ids = new ITransfer[] { cid.Multi, cid.Uni };
             foreach (var id in ids)
-                Client.Subscribe<I>(id, OnRequest<I, O>);
+                Client.Subscribe<I>(id, OnRequest);
         }
 
-        void OnRequest<I, O>(IIdentity sender, I req)
-            where I : IRequest where O : IResponse
+        void OnRequest<I>(IIdentity sender, I req) where I : IRequest
         {
-            var handler = AddinManager.GetExtensionObjects<IMessageHandler<I, O>>().First();
+            var handler = (IRequestHandler<I>)Handlers[typeof(I)];
             var resp = handler.Process(req);
             Client.Publish(sender.Uni, resp);
         }
