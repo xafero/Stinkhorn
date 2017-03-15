011225f836517f428a502242bdca5f69bed8be34
xafero <georg.berendt@outlook.com>
2017-03-16 00:03:56 +0100

    Working on API

--- Stinkhorn.VFS.API/MountRequest.cs
+++ Stinkhorn.VFS.API/MountRequest.cs
@@ -1,17 +1,56 @@
-﻿using System.Collections.Generic;
-using Stinkhorn.API;
+﻿using Stinkhorn.API;
+using static System.Environment;
 
 namespace Stinkhorn.VFS.API
 {
     [RequestDesc(Category = nameof(VFS))]
     public class MountRequest : IRequest
     {
-
+        public FsType Type { get; set; } = FsType.Auto;
+        public SpecialFolder Source { get; set; } = SpecialFolder.MyComputer;
+        public string Target { get; set; } = "/";
     }
 
     [ResponseDesc]
-    public class MountResponse : IResponse
+    public class MountResponse : IResponse, IFolder
     {
-        public IDictionary<string, string> Drives { get; set; }
+        public IFile[] Files { get; set; }
+        public IFolder[] Folders { get; set; }
+    }
+
+    public enum FsType
+    {
+        Auto
+    }
+
+    public interface IEntry
+    {
+
+    }
+
+    public interface IFile : IEntry
+    {
+
+    }
+
+    public interface IFolder : IEntry
+    {
+        IFile[] Files { get; }
+        IFolder[] Folders { get; }
+    }
+
+    public abstract class VfsEntry : IEntry
+    {
+
+    }
+
+    public class VfsFile : VfsEntry, IFile
+    {
+
+    }
+
+    public class VfsFolder : VfsEntry, IFile
+    {
+
     }
 }
--- Stinkhorn.VFS.Shared/NetMountHandler.cs
+++ Stinkhorn.VFS.Shared/NetMountHandler.cs
@@ -2,6 +2,7 @@
 using Stinkhorn.VFS.API;
 using System.IO;
 using System.Linq;
+using static System.Environment;
 
 namespace Stinkhorn.VFS.Shared
 {
@@ -10,11 +11,29 @@ namespace Stinkhorn.VFS.Shared
     {
         public IResponse Process(MountRequest input)
         {
-            var allDrives = DriveInfo.GetDrives();
-            var drives = allDrives.Where(d => d.DriveType == DriveType.Fixed
-                && d.IsReady).ToDictionary(k => k.RootDirectory.FullName,
-                v => v.VolumeLabel);
-            return new MountResponse { Drives = drives };
+            var specialFld = input.Source;
+            string[] dirs;
+            string[] files;
+            if (specialFld == SpecialFolder.MyComputer)
+            {
+                var allDrives = DriveInfo.GetDrives();
+                var drives = allDrives.Where(d => d.DriveType == DriveType.Fixed
+                        && d.IsReady).ToDictionary(k => k.RootDirectory.FullName,
+                        v => v.VolumeLabel);
+                dirs = drives.Values.ToArray();
+                files = new string[0];
+            }
+            else
+            {
+                var folder = GetFolderPath(specialFld, SpecialFolderOption.Create);
+                dirs = Directory.GetDirectories(folder);
+                files = Directory.GetFiles(folder);
+            }
+            return new MountResponse
+            {
+                Directories = dirs.Select(d => new VirtualDirectory()),
+                Files = files.Select(f => new VirtualFile())
+            };
         }
     }
 }
