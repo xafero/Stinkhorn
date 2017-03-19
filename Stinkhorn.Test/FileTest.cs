f5e60394d46f00de25457a06f8c04547d4db5255
xafero <georg.berendt@outlook.com>
2017-03-19 20:13:39 +0100

    Added test for vfs

---
+++ Stinkhorn.Test/FileTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Stinkhorn.VFS.API;
+using Stinkhorn.VFS.Shared;
+using System.Linq;
+using static System.Environment;
+
+namespace Stinkhorn.Test
+{
+    [TestFixture]
+    public class FileTest
+    {
+        [Test]
+        public void TestMount()
+        {
+            var handler = new NetMountHandler();
+            var mr = new MountRequest
+            {
+                Type = FsType.Auto,
+                Source = SpecialFolder.MyComputer,
+                Target = "/drives"
+            };
+            var rr = handler.Process(mr) as MountResponse;
+            Assert.AreEqual("MyComputer", rr.Source + "");
+            Assert.AreEqual("/drives", rr.Target);
+            Assert.AreEqual(0, rr.Files.Count());
+            Assert.IsTrue(rr.Folders.Count() >= 1);
+        }
+    }
+}
